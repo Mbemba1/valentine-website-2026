03de68327c03c3d689be46b90bb6dd6e864ebe96
Mbemba1 <badjiembemba@gmail.com>
2026-02-15 04:02:17 +0700

    Update music URL for streaming

--- config.js
+++ config.js
@@ -105,7 +105,7 @@ if (CONFIG.celebration.image && CONFIG.celebration.image.enabled) {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dzkmph82e/video/upload/v1771102817/videoplayback_kgl8fh.mp4", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
